c450a1110f26c14c09f4c84c8e146130b780e29a
kcko <kcko2924@gmail.com>
2024-07-21 18:59:43 +0900

    MyAutoConfigImportSelector

--- src/main/java/com/ryon/helloboot/HellobootApplication.java
+++ src/main/java/com/ryon/helloboot/HellobootApplication.java
@@ -1,29 +1,11 @@
 package com.ryon.helloboot;
 
+import com.ryon.config.MySpringBootApplication;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
-import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.DispatcherServlet;
 
-//@SpringBootApplication
-@Configuration
-@ComponentScan
+
+@MySpringBootApplication
 public class HellobootApplication {
-	@Bean
-	public ServletWebServerFactory servletContainer() {
-		return new TomcatServletWebServerFactory();
-	}
-
-	@Bean
-	public DispatcherServlet dispatcherServlet() {
-		return new DispatcherServlet();
-	}
-
-
 	public static void main(String[] args) {
 		SpringApplication.run(HellobootApplication.class, args);
 	}
